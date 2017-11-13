84cfe16aa7e602e78d03ce98665535ad493f106a
Greg Vorobyev <gvorobjev@gmail.com>
2017-11-12 23:38:29 -0500

    credit jsx

--- client/src/views/lobby/Credit.jsx
+++ client/src/views/lobby/Credit.jsx
@@ -4,7 +4,7 @@ import {observer} from 'mobx-react';
 const Credit = observer(({}) => {
   return (
     <div className='right mt2 h6'>
-      built by <a href='http://catchsparrow.com/gregory-vorobyev/'>Greg</a> | <a href='https://github.com/hazelsparrow/ncoup' className='text-primary'><i class="fa fa-github" aria-hidden="true"></i></a> | <a href='https://www.linkedin.com/in/gregoryvorobyev/'><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+      built by <a href='http://catchsparrow.com/gregory-vorobyev/'>hazelsparrow</a> | <a href='https://github.com/hazelsparrow/ncoup' className='text-primary'><i class="fa fa-github" aria-hidden="true"></i></a> | <a href='https://www.linkedin.com/in/gregoryvorobyev/'><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
     </div>
   );
 });
