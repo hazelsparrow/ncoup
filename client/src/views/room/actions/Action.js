6387521cebb69e7a3e8c8002b8e041bd40e358f6
Greg Vorobyev <gvorobjev@gmail.com>
2017-11-26 18:42:26 -0500

    wip

--- client/src/views/room/actions/Action.js
+++ client/src/views/room/actions/Action.js
@@ -3,18 +3,25 @@ import {observer} from 'mobx-react';
 import ActionButton from './ActionButton';
 import ACTION_TYPES from '../../../game/actionTypes';
 
-const Action = observer(({action, store}) => {
-  switch (action.type) {
+function getClassName(actionType) {
+  switch (actionType) {
     case ACTION_TYPES.START_GAME:
+      return 'Action--start-game';
     case ACTION_TYPES.ABANDON_GAME:
-      return (
-        <ActionButton onClick={() => action.execute(store.game)} disabled={action.disabled}>
-          {action.name}
-        </ActionButton>
-      );
+      return 'Action--abandon-game'
     default:
-      throw new Error(`Action type ${action.type} is not supported.`);
+      return '';
   }
+}
+
+const Action = observer(({action, store}) => {
+  return (
+    <ActionButton onClick={() => action.execute(store.game)}
+                  className={getClassName(action.type)}
+                  disabled={action.disabled}>
+      {action.name}
+    </ActionButton>
+  );
 });
 
 export default Action;
--- client/src/views/room/actions/ActionButton.js
+++ client/src/views/room/actions/ActionButton.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {observer} from 'mobx-react';
 import {Button} from '../../../components';
+import './index.css';
 
-const ActionButton = observer(({onClick, children, disabled = false}) => {
+const ActionButton = observer(({onClick, children, className, disabled = false}) => {
   return (
-    <Button onClick={onClick} disabled={disabled}>
+    <Button onClick={onClick}
+            disabled={disabled}
+            className={`Btn ${className}`}>
       {children}
     </Button>
   );
